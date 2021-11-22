b2036572072312ce677b949c1115e81e3751a5c7
yashav01 <vy2388@gmail.com>
2021-11-22 14:13:00 -0500

    finished product with JS button animation

---
+++ resources/JavaScript/main.js
@@ -0,0 +1,33 @@
+
+window.onmouseover = e => {
+    let btnID = e.target.id
+    if(btnID === 'btnServices' || btnID === 'btnAbout' || btnID === 'btnContact'){
+        e.target.style.boxShadow = '0 10px 15px blue';
+        e.target.style.transition = 'all 1s';
+    };
+};
+
+window.onmouseout = e => {
+    let btnID = e.target.id
+    if(btnID === 'btnServices' || btnID === 'btnAbout' || btnID === 'btnContact'){
+        e.target.style.boxShadow = '';
+        e.target.style.transition = 'all 1s';
+    };
+};
+
+window.onmousedown = e => {
+    let btnID = e.target.id
+    if(btnID === 'btnServices' || btnID === 'btnAbout' || btnID === 'btnContact'){
+        e.target.style.boxShadow = '0 10px 15px red';
+        e.target.style.transition = 'all 0.25s';
+    };
+};
+
+window.onmouseup = e => {
+    let btnID = e.target.id
+    if(btnID === 'btnServices' || btnID === 'btnAbout' || btnID === 'btnContact'){
+        e.target.style.boxShadow = '0 10px 15px blue';
+        e.target.style.transition = 'all 0.75s';
+    };
+};
+
